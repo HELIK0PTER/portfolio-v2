c000388f0d9c5840e9b604d701595cd46aa762c9
Matheus Kops Guedes <matheuskopsguedes@gmail.com>
2025-06-21 01:51:13 +0200

    probleme d'url authorization pour l'auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,7 +4,7 @@ import { prisma } from "@/lib/prisma";
 
 export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET || "bf5a77420e3f66b1ec1d02f214eff992539b748c610ae79d3043878f2667d0d0",
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
